67357bc2ea2b490273e8f4cebf9aa81fce668db9
VERET Salif <salif.veret@telecomnancy.eu>
2026-05-05 22:06:23 +0200

    initial commit: project structure

---
+++ src/proxy.h
@@ -0,0 +1,11 @@
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 4096
+
+//client-server link connection struct
+typedef struct {
+    int client_fd;
+    int server_fd;       //socket to localhost:8080
+    int is_active;       //1 if active, else 0
+} ConnectionState;
+
+ConnectionState connections[MAX_CLIENTS];
